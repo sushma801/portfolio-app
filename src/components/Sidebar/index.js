d80d9da87f7edbb1528d76ad1d7d13cdab1b4f2d
sushma Kumari <sushmashaw801@gmail.com>
2023-03-26 18:27:41 +0530

    modified the changes.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,29 +1,27 @@
 import './index.scss';
 import { Link, NavLink } from 'react-router-dom';
-import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
+import DisplayPicture from '../../assets/images/dp.jpeg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faEnvelope, faHome,  faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () =>{
     return (
         <div className="nav-bar">
             <Link className='logo' to='/'>
-                <img src={LogoS} alt="logo"/>
-                <img className="sub-logo" src={LogoSubtitle} alt="slobodan"/>
+                <img src={DisplayPicture} alt="logo"/>
             </Link>
             <nav>
-                <NavLink exact="true" activeclassname="active" to='/'>
+                <NavLink exact="true"  to='/'>
                     <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
                 </NavLink>
 
-                <NavLink exact="true" activeclassname="active" className="about-link" to='/about'>
-                    <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+                <NavLink exact="true"  className="about-link" to='/about'>
+                    <FontAwesomeIcon icon={faBars} color="#4d4d4e"/>
                 </NavLink>
 
-                <NavLink exact="true" activeclassname="active" className="contact-link" to='/contact'>
-                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+                <NavLink exact="true"  className="contact-link" to='/contact'>
+                    <FontAwesomeIcon icon={faUserCircle} color="#4d4d4e"/>
                 </NavLink>
             </nav>
             <ul>
@@ -37,6 +35,12 @@ const Sidebar = () =>{
                     <a target="_blank" rel='noreferre' href="https://github.com/sushma801">
                         <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
                     </a>
+                </li>
+                <li>
+                    <div className='tooltip'>
+                        <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+                        <span className='tooltiptext'>sushmashaw801@gmail.com</span>
+                    </div>
                 </li>
             </ul>
         </div>
